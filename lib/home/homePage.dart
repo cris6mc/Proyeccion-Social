e56d7d0bd52f22c1c29dcb4c9bb4130ad4bdc3af
Cristian Machado <cris6mc@gmail.com>
2024-05-22 15:28:50 -0500

    add files to host

--- lib/home/homePage.dart
+++ lib/home/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,20 +21,27 @@ class _HomePageState extends State<HomePage> {
         Container(
           color: Colors.grey,
           height: 150,
-          child: const Row(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Column(
                 children: [
-                  Text('Bienvenidos'),
-                  SizedBox(
+                  const Text('Bienvenidos'),
+                  const SizedBox(
                     width: 450,
                     child: Text(
                         'Somos un colegio peruano-británico ubicado en un hermoso campus junto a una laguna. Ofrecemos los programas del Bachillerato Internacional y es nuestra meta “formar jóvenes solidarios, informados y ávidos de conocimiento, capaces de contribuir a crear un mundo mejor y más pacífico, en el marco del entendimiento mutuo y el respeto intercultural" (Misión del IB).'),
-                  )
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        FirebaseFirestore.instance
+                            .collection('pruebas')
+                            .add({'nombre': 'Juan'});
+                      },
+                      child: const Text('Enviar nombre a db')),
                 ],
               ),
-              Center(
+              const Center(
                 child: Icon(Icons.image),
               )
             ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:san_pedro_de_yauyo/home/mainPage.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+// ...
+
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
